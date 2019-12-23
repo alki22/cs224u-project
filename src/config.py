cc2c749cc37b96bdc4909f4cd61fc8574969e9fe
alki22 <alki.collias@gmail.com>
2019-12-23 18:08:53 -0300

    Added CR classifier model constants

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ class Config:
     BASE_DIR = Path(realpath(join(dirname(realpath(__file__)), '..')))
     RUNS_DIR = Path(join(BASE_DIR, "runs"))
     DATA_DIR = Path(join(BASE_DIR, "data"))
+    MODELS_DIR = Path(join(BASE_DIR, "models"))
     CORPUS_FILE = Path(join(DATA_DIR, "new_dataset_preprocessed.csv"))
     FASTTEXT_FILE = Path(join(DATA_DIR, "fasttext", "wiki.en.vec")) 
     CORE_NLP_DIR = join(BASE_DIR, "stanford-corenlp-full-2018-10-05")
@@ -26,9 +27,14 @@ class Config:
     QUESTION_AND_PAYMENT_INFO_DATASET_FILE = _lift_split_file("%s_question_and_payment_info_dataset.csv")
     QUESTION_AND_TUTOR_COUNTRY_DATASET_FILE = _lift_split_file("%s_question_and_tutor_country_dataset.csv")
     QUESTION_AND_TUTOR_SCORE_DATASET_FILE = _lift_split_file("%s_question_and_tutor_score_dataset.csv")
+    QUESTION_AND_CR_PREDICTION_DATASET_FILE = _lift_split_file("%s_question_and_cr_prediction_dataset.csv")
     QUESTION_TEXT_AND_RESPONSE_TEXT_DATASET_FILE = _lift_split_file("%s_question_text_and_response_text_dataset.csv")
     QUESTION_AND_SENTIMENT_DATASET_FILE = _lift_split_file("%s_question_and_sentiment_dataset.csv")
     LABEL_COUNTS_DATASET_FILE = _lift_split_file("%s_label_counts_dataset.csv")
+    CR_CLASSIFIER_DATASET_FILE = Path(join(DATA_DIR, "question_text_and_response_text_dataset.csv"))
+
+    LR_CR_CLASSIFIER_MODEL = Path(join(MODELS_DIR, "cr_classifier", "logistic", "clf.sav"))
+    SVM_CR_CLASSIFIER_MODEL = Path(join(MODELS_DIR, "cr_classifier", "svm", "clf.sav"))
 
     MAX_CONTEXT_WINDOW_SIZE = 10
     QUESTION_AND_CONTEXT_WINDOW_DATASET_FILE = _lift_split_file("%s_question_and_context_window_dataset.csv")
